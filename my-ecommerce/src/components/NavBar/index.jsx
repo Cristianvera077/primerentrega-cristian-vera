af912172e4ba914d624216ce1e1bf4d855bd03aa
cristian vera <cristianvera077@hotmail.com>
2023-02-14 22:12:35 -0300

    first commit

---
+++ my-ecommerce/src/components/NavBar/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+export const NavBar = () => {
+    return ( <div className="container">
+
+    <nav className="nav">
+    
+    <div className="nav_brand">
+    
+    <a className="nav_link" href="#">Vikinga indumentaria</a>
+    
+    </div>
+    
+    <ul className="nav_list">
+    
+    <li>
+    
+    <a className="nav_link" href="#">Remera</a>
+    
+    </li> <li>
+    
+    <a className="nav_link" href="#">Pantalon</a>
+    
+    </li>
+    
+    <li>
+    
+    <a className="nav_link" href="#">buzos</a>
+    
+    </li>
+    </ul>
+    </nav>
+    </div>
+       
+    );
+    
+}
+export default NavBar
